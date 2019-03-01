6f8db30c2f8e009b44b0fa42474968e4d5eca03f
Michael Enriquez <michael.ozquez@gmail.com>
2019-03-01 11:09:16 -0600

    pruebas base

--- src/app/routes/bonos.js
+++ src/app/routes/bonos.js
@@ -5,7 +5,7 @@ module.exports = app =>{
     const connection = dbConnection();   
     
     app.get('/', (req, res) => {
-        connection.query('SELECT * FROM cat_tiposBonos;', (err, result) =>{
+        connection.query('SELECT t.* FROM efo_bonos.cat_tiposBonos;', (err, result) =>{
             console.log(result);
             
             // res.render('bonos/bonos', {
